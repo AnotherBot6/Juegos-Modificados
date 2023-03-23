6e7bd9cbf080a4cc4b387ad5410622f34341947c
Charles M <carlolakase@outlook.com>
2023-03-22 19:47:23 -0600

    paint.py

--- paint.py
+++ paint.py
@@ -43,12 +43,40 @@ def circle(start, end):
 
 def rectangle(start, end):
     """Draw rectangle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(4):
+        if count%2 != 0: # Hace que los trazos pares sean mas largos que los impares
+            forward(end.x - start.x +65)
+            left(90)
+        else:
+            forward(end.x - start.x)
+            left(90)
+    
+
+    end_fill()
+
 
 
 def triangle(start, end):
-    """Draw triangle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    
+    forward(end.x - start.x)
+    left(120)
+    forward(end.x - start.x)
+    left(120)
+    forward(end.x - start.x)
+
+    
+
+    end_fill()
+
 
 
 def tap(x, y):
@@ -88,4 +116,4 @@ onkey(lambda: store('shape', circle), 'c')
 onkey(lambda: store('shape', rectangle), 'r')
 onkey(lambda: store('shape', triangle), 't')
 done()
-# hola mundo
+
